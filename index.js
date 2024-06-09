0282fa0ceb9dba94a3d7610bc22a664a7e54f978
akkhara <p.akkhara@gmail.com>
2024-06-09 21:47:13 +0700

    add hourly check

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const cron = require('node-cron');
 const {exec} = require('child_process');
 const lineNotify = require('line-notify-nodejs')('bc9ZH7slbMEnQ5mTQ8MKFetKF7oxVNr10HP8rHyvlIY');
 const os = require('os');
-cron.schedule('0 8,12 * * *', function() {
-  console.log('start job');
+cron.schedule('30 8,20 * * *', function() {
+  console.log('daily check');
   exec('/usr/local/go/bin/pandocli query status', (error, stdout, stderr) => {
     if (error) {
+      lineNotify.notify({
+        message: `${os.hostname()}\nError\n${error.message}`
+      });
       console.error('Error', error.message);
     } else if (stderr) {
+      lineNotify.notify({
+        message: `${os.hostname()}\nStdErr\n${stderr}`
+      });
       console.error('StdErr', stderr);
     } else {
       var status = JSON.parse(stdout);
@@ -17,18 +23,38 @@ cron.schedule('0 8,12 * * *', function() {
 latest finalized block: ${status.latest_finalized_block_height}
 latest finalized time: ${dt.getFullYear()}-${dt.getMonth() + 1}-${dt.getDate()} ${dt.getHours()}:${dt.getMinutes()}:${dt.getSeconds()}
 syncing: ${status.syncing}`;
-      //if (timestamp - status.latest_finalized_block_time >= 300) {
-        //line notification
+      lineNotify.notify({
+        message: msg
+      });
+      console.log(msg);
+    }
+  });
+});
+cron.schedule('0 * * * *', function() {
+  console.log('hourly check');
+  exec('/usr/local/go/bin/pandocli query status', (error, stdout, stderr) => {
+    if (error) {
+      lineNotify.notify({
+        message: `${os.hostname()}\nError\n${error.message}`
+      });
+      console.error('Error', error.message);
+    } else if (stderr) {
+      lineNotify.notify({
+        message: `${os.hostname()}\nStdErr\n${stderr}`
+      });
+      console.error('StdErr', stderr);
+    } else {
+      var status = JSON.parse(stdout);
+      const timestamp = Date.now() / 1000;
+      if (timestamp - status.latest_finalized_block_time >= 300) {
+        var dt = new Date(status.latest_finalized_block_time * 1000);
+        var msg = `${os.hostname()}\nStoped syncing since: ${dt.getFullYear()}-${dt.getMonth() + 1}-${dt.getDate()} ${dt.getHours()}:${dt.getMinutes()}:${dt.getSeconds()}`;
         lineNotify.notify({
           message: msg
         });
-      //} else {
-        console.log(msg);
-      //};
-      /*console.log(`${os.hostname()}
-latest finalized block: ${status.latest_finalized_block_height}
-latest finalized time: ${dt.getFullYear()}-${dt.getMonth() + 1}-${dt.getDate()} ${dt.getHours()}:${dt.getMinutes()}:${dt.getSeconds()}
-syncing: ${status.syncing}`);*/
+      } else {
+        console.log('synced');
+      }
     }
   });
 });
